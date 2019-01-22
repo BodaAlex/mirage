ab2a049b23ec548110c5294fae5a95ef2b96e4cc
BodaAlex <46644709+BodaAlex@users.noreply.github.com>
2019-01-22 15:14:18 +0200

    Adding files to repository

---
+++ script.js
@@ -0,0 +1,32 @@
+// SCRIPT FOR BIG IMAGES ON CLICK------------------PULA------------------------>
+console.log("alert");
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementByClassName('port-item');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Code for GOOGLE MAP-------------------------------------------------------->
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
